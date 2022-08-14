04113f5e9f7bc55f8a0d2e0b0f4437054bf90d18
july-12 <seventanyb@gmail.com>
2022-08-14 21:33:01 +0800

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const outDir = resolve(__dirname, 'docs')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'hometest',
+  base: '/homepage/',
   root,
   plugins: [react()],
   build: {
